79ece9584dada84fa9413f44c0540f33f7d84330
iftialghalib <niceoneforyou11@gmail.com>
2025-06-02 10:06:04 +0700

    prototype regist

---
+++ Backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // 1. validator 
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:50',
+            'email' => 'required|email|max255|unique:users',
+            'password' => 'required|min:8'
+        ]);
+
+        // 2. check validator error 
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+
+        ]);
+
+        if ($user) {
+            return response()->json([
+                'success' => true,
+                'message' => 'User created Successfully',
+                'data' => $user
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'User creation fail',
+        ], 409); // conflict
+    }
+}
--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -15,9 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            SantriSeeder::class,
+            ClassroomsSeeder::class,
+            DormsSeeder::class,
+            SubjectsSeeder::class,
+            DormAssignmentsSeeder::class,
+            GradesSeeder::class,
         ]);
     }
 }
